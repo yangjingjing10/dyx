bee264e97a23f46ae6c5f0c6940ed0a1e6e9a714
yangjingjing10 <yjj1345391577@gmail.com>
2025-10-05 17:51:44 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,50 @@
+// Service Worker for 多云熊小手机 PWA
+const CACHE_NAME = '多云熊小手机-v1';
+const urlsToCache = [
+  '/',
+  '/多云熊小手机_桌面第二页.html',
+  '/manifest.json'
+];
+
+// 安装事件
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('缓存已打开');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// 激活事件
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('删除旧缓存:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// 拦截请求
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // 如果缓存中有，返回缓存
+        if (response) {
+          return response;
+        }
+        // 否则从网络获取
+        return fetch(event.request);
+      }
+    )
+  );
+});
